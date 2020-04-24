9992d61c2036b99ccdf62ec35e38b251c6105eb6
puss <673256903@qq.com>
2020-04-24 14:28:25 +0800

    添加项目文件。

---
+++ Forms.GetGuid/Form.Home.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Forms.GetGuid
+{
+    partial class form_Home
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Get = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Get
+            // 
+            this.btn_Get.Location = new System.Drawing.Point(60, 26);
+            this.btn_Get.Name = "btn_Get";
+            this.btn_Get.Size = new System.Drawing.Size(75, 23);
+            this.btn_Get.TabIndex = 0;
+            this.btn_Get.Text = "获取Guid";
+            this.btn_Get.UseVisualStyleBackColor = true;
+            this.btn_Get.Click += new System.EventHandler(this.btn_Get_Click);
+            // 
+            // form_Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(198, 85);
+            this.Controls.Add(this.btn_Get);
+            this.Name = "form_Home";
+            this.Text = "获取Guid";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Get;
+    }
+}
+
---
+++ Forms.GetGuid/Form.Home.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms.GetGuid
+{
+    public partial class form_Home : Form
+    {
+        public form_Home()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Get_Click(object sender, EventArgs e)
+        {
+            Guid ID = Guid.NewGuid();
+            Clipboard.SetDataObject(ID.ToString());
+        }
+    }
+}
